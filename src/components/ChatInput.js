f85bb860ddcbd951178024404316369334a1eeb6
Michael Taggart <mtaggarthawk@gmail.com>
2020-11-19 10:00:36 -0700

    Morning Review Completed ' '

--- src/components/ChatInput.js
+++ src/components/ChatInput.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
 
 export default class ChatInput extends Component {
+
+	constructor() {
+		super();
+
+		this.state = {
+			input: ""
+		}
+		this.handleInput = this.handleInput.bind(this)
+	}
+
+	handleInput(e) {
+		this.setState({ input: e.target.value })
+	}
+
+	handleClick() {
+
+	}
+
 	render() {
 		return (
 			<div id="chat-input">
-				<input placeholder='type something...' />
-				<button id='chat-submit'>Ask!</button>
+				<input onChange={e => this.handleInput(e)} placeholder='type something...' />
+				<button onClick={() => this.props.processMessage(this.state.input)} id='chat-submit'>
+					Ask!
+				</button>
 			</div>
 		);
 	}
